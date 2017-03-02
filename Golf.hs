3993319cc91385cb5abefadda30fd8a75ea5f4bb
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2017-03-02 15:47:20 +0530

    Prasenjit | homework3 second problem

--- Golf.hs
+++ Golf.hs
@@ -6,4 +6,17 @@ findEveryNth :: [a] -> Int -> [a]
 findEveryNth list n = map head $ (chunksOf n) $ drop (pred n) list
 
 skips :: [a] -> [[a]]
-skips list = map (findEveryNth list) [1..(length list)] 
+skips list = map (findEveryNth list) [1..(length list)]
+
+lookUpLocalMaxima :: [Integer] -> [Integer] -> [Integer]
+lookUpLocalMaxima list result
+  | (<) (length list) 3 = result
+  | otherwise = if and [((>) second first), ((>) second third)]
+                then lookUpLocalMaxima (tail list) (result ++ [second])
+                else lookUpLocalMaxima (tail list) result
+  where first = head list
+        second = head . (drop 1) $ list
+        third = head . (drop 2) $ list
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima list = lookUpLocalMaxima list []
--- GolfTest.hs
+++ GolfTest.hs
@@ -23,3 +23,9 @@ module GolfTest where
 
       it "gives empty list for empty list" $ do
         length (skips []) `shouldBe` 0
+
+    describe "localMaxima" $ do
+      it "should create the list of maximu from a given list" $ do
+        localMaxima [2,9,5,6,1] `shouldBe` [9,6]
+        localMaxima [2,3,4,1,5] `shouldBe` [4]
+        localMaxima [1,2,3,4,5] `shouldBe` []
